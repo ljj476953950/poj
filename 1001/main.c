a1bf8361473bf6f5196437878a262c6109d942e9
LJJ <476953950@qq.com>
2015-09-17 21:05:07 +0800

    1001 v2
    有未知bug

--- 1001/main.c
+++ 1001/main.c
@@ -13,21 +13,31 @@ int my_pow(int a,int b)
 int main()
 {
 	char input[6],output[32*4];
-	int i,j,n,point=0,point_be=0,point_out;
+	int i,j,n,point=0,point_be=0,point_out,point_out2,int_be=1,end=0,zero_num=0,zero_num2;
+	int first = 1;
 	int R=0;
 	int R_tmp[32]={1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},tmp,tmp_c;
 	scanf("%s %d",input,&n);	
 	for(i=0;i<6;i++)
 	{
+		if(point_be && input[i]!='0')
+			{
+			int_be=0;
+			zero_num=0;
+			}
+		if(point_be && input[i]=='0')
+			zero_num +=1;
 		if(input[i]=='.')
 		{
 			point = i;
 			point_be = 1;
 		}
 		else
-			R += (input[i] - '0')*my_pow(10,4-i+point_be);	
+			R += (input[i] - '0')*my_pow(10,4-i+point_be);
+		
 	}
 	point_out = (5-point) * n;
+	zero_num = zero_num * n;
 	R_tmp[0] = 1;
 	for(i=0;i<n;i++)
 	{
@@ -41,31 +51,96 @@ int main()
 			tmp_c = tmp / 10000;
 		}
 	}
-	int first = 1;
+	
 	for(i=31;i>=0;i--)
 	{
+		if(end)break;
+
 
 		if(first)
 		{
-			if((R_tmp[i]!=0) || ((point_out < i*4) &&(point_out >= (i-1)*4) ))
+			if((R_tmp[i]!=0) || ((point_out < (i+1)*4+1) &&(point_out >= i*4+1) ))
 			{
-				first =0;
-				point_out -= (i-1)*4);
-				for(j=3;j>=0;j--)
+				zero_num2 = zero_num-i*4-1;
+				if((point_out < (i+1)*4+1) &&(point_out >= i*4+1) )
 				{
-					if(point_out == j)	
+					point_out2 = point_out-i*4-1;
+					for(j=3;j>=0;j--)
 					{
-						printf(".");
-					}
-					if(R_tmp[i])
+						if(point_out2 == j)	
+						{
+							if(int_be)
+							{
+								end = 1;
+								break;
+							}
+							printf(".");
+							
+							first=0;
+						}
+						if(zero_num2 == j)	
+						{
+							end = 1;
+							break;
+						}
+						tmp=(R_tmp[i]/my_pow(10,j))%10;
+						if(tmp)
+						{
+							first=0;
+						}
+						if(first==0)
+							printf("%d",tmp);
+					}	
 				}
+				
+				else 
+				{
+					printf("%d",R_tmp[i]);
+					first=0;
+				}
+					
+		
 			}
 		}
+		else 
+		{
+		if(((point_out < (i+1)*4+1) &&(point_out >= i*4+1) ))
+		{
+			point_out2 = point_out-i*4;
+			for(j=3;j>=0;j--)
+			{
+				if(point_out2-1 == j)	
+				{
+					if(int_be)
+					{
+						end = 1;
+						break;
+					}
+						
+					printf(".");
+				}
+				printf("%d",(R_tmp[i]/my_pow(10,j))%10);
+			}
+		}
+		else if((zero_num < (i+1)*4+1) &&(zero_num >= i*4+1) )
+		{
+			zero_num = zero_num-i*4;
+			for(j=3;j>=0;j--)
+			{
+				if(zero_num-1 == j)	
+				{
+					end = 1;
+					break;
+				}
+				printf("%d",(R_tmp[i]/my_pow(10,j))%10);
+			}
+			
+		}
+		else 
+			printf("%04d",R_tmp[i]);
 		
-		
-		printf("%04d",R_tmp[i]);
+		}
 	}
-	
 	
 	
 	return 0;
